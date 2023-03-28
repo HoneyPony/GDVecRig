ec30ddae06ccd37ef6aac017717d145fbf9990bb
HoneyPony <honeyponygames@gmail.com>
2023-03-28 17:52:25 -0400

    Remove outdated code, seemingly fix project reload issue
    The project seemingly no longer crashes when being reloaded, which
    is great.

--- addons/gdvecrig/gdvecrig.gd
+++ addons/gdvecrig/gdvecrig.gd
@@ -56,11 +56,6 @@ func _forward_canvas_gui_input(event: InputEvent) -> bool:
 func _make_visible(visible):
 	pass
 
-
-var button_draw
-var button_paint
-var button_group
-
 var dock
 
 var bone_list: ItemList
@@ -68,30 +63,12 @@ var bone_list: ItemList
 func _enter_tree():
 	Engine.register_singleton("GDVecRig", self)
 	
-	button_group = ButtonGroup.new()
-	
-	button_draw = Button.new()
-	button_draw.toggle_mode = true
-	button_draw.text = "Draw"
-	button_draw.button_group = button_group
-	
-	button_paint = Button.new()
-	button_paint.toggle_mode = true
-	button_paint.text = "Paint"
-	button_paint.button_group = button_group
-	
-	
-	
-	add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, button_draw)
-	add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, button_paint)
-	
-	
 	# Load the dock scene and instantiate it.
 	dock = preload("res://addons/gdvecrig/Vector Editing.tscn").instantiate()
 
 	# Add the loaded scene to the docks.
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
-	bone_list = dock.get_node("TabContainer/Weight Painting/BoneList")
+	bone_list = dock.get_node("%BoneList")
 	bone_list.connect("item_selected", _on_bone_list_selected)
 	# Note that LEFT_UL means the left of the editor, upper-left dock.
 	# Initialization of the plugin goes here.
@@ -99,12 +76,10 @@ func _enter_tree():
 
 
 func _exit_tree():
-	Engine.unregister_singleton("GDVecRig")
-	remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, button_draw)
-	remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, button_paint)
-	
 	remove_control_from_docks(dock)
 	# Erase the control from the memory.
 	dock.free()
 	# Clean-up of the plugin goes here.
+	
+	Engine.unregister_singleton("GDVecRig")
 	pass
